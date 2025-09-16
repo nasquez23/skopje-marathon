baf1b188ea5269b5e9a5a8332d5bf23a9cbd9df2
nasquez23 <ivannaskov15@gmail.com>
2025-09-16 20:56:37 +0200

    chore: initial commit

---
+++ backend/src/main/java/com/skopjemarathon/SkopjeMarathonApplication.java
@@ -0,0 +1,13 @@
+package com.skopjemarathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkopjeMarathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkopjeMarathonApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/skopjemarathon/SkopjeMarathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skopjemarathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkopjeMarathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
